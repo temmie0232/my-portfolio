332945cb2c4100262c419b517e4e40b4bb208d23
temmie0232 <temmie0232@gmail.com>
2024-11-27 09:33:24 +0900

    add: サイドナビゲーションの型定義の作成

--- src/features/home/components/Portfolio.tsx
+++ src/features/home/components/Portfolio.tsx
@@ -1,9 +1,16 @@
 "use client"
 import React from 'react';
 
+type SectionId = 'top' | 'about' | 'app' | 'skills';
+
 const Portfolio = () => {
+
+    const [activeSection, setActiveSection] = React.useState<SectionId>('top');
+
+    const sections: SectionId[] = ['top', 'about', 'app', 'skills'];
+
     return (
-        <div className="min-h-screen">
+        <div className="flex min-h-screen">
             <h1>Hello World</h1>
         </div>
     );
